4d15e72ea729b1b9c5553b3e8dea02a73b0ce637
Rick Arcanjo <Ricardo.cassettari07@gmail.com>
2023-03-10 14:33:52 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Poema-Concreto");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.Write(@"u m
+     m o v i
+     mento
+   compondo
+  além
+         da
+ nuvem
+    um
+   campo
+      de
+   combate
+     mira
+   gem
+      ira
+         de
+    um
+      horizonte
+puro
+   num
+     mo
+     mento
+ vivo
+ ;");
